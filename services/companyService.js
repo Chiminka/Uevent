b0ec6c25fc6899e51d365bb95e32ad6652523ade
Chiminka <58224752+Chiminka@users.noreply.github.com>
2023-04-17 22:26:28 +0200

    Add files via upload

--- services/companyService.js
+++ services/companyService.js
@@ -248,6 +248,7 @@ const updatePromo = async (req, res) => {
     return { message: "no companies" };
 
   const promo_code = await Promocode.findOne({ company: req.params.id });
+
   if (!promo_code) {
     // создаем новый объект Date с текущей датой и временем
     var currentDate = new Date();
@@ -300,11 +301,48 @@ const updatePromo = async (req, res) => {
       }
     }
     return {
-      message: "Promo was created and sent to subs",
+      message: "Promo was created",
     };
   } else {
+    const company = await Company.findById(req.params.id);
+
+    // фильтруем пользователей, чтобы оставить только тех, у кого есть подписки на компанию
+    const usersWithSubscription = await User.find({
+      subscriptions_companies: req.params.id,
+    });
+
+    const randomUsers = usersWithSubscription
+      .slice()
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 5);
+    // получаем 2 случайных элемента из массива usersWithSubscription
+
+    const promocode = await Promocode.findOne({ company: req.params.id });
+
+    if (!promocode) {
+      return { message: "Firstly create a new promo" };
+    }
+
+    // Для каждого пользователя добавляем промокод, если его нет в массиве промокодов пользователя
+    for (const user of randomUsers) {
+      if (!promocode.users.includes(user.id)) {
+        promocode.users.push(user.id);
+        await promocode.save();
+
+        // Отправляем письмо с промокодом пользователю
+        mailTransport().sendMail({
+          from: process.env.USER,
+          to: user.email,
+          subject: `You got a promo-code! From site "Let's go together"`,
+          html: `<h1>${user.username}, you got a promo-code for events by company ${company.company_name}</h1>
+        <h2>Put it in and get 4% discount!</h2>
+        <h2>${promocode.promo_code}</h2>
+        <h3>Promo code is valid until ${promocode.expiration_date}</h3>`,
+        });
+      }
+    }
     return {
-      message: "Your promo is still alive",
+      message: "Your promo is still alive and we sent to subs",
     };
   }
 };
